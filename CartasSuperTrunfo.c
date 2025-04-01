02cb0e819858cecd7f2f90b95e3840d8f625768d
Elito00 <elito.5.0liveira@gmail.com>
2025-04-01 23:21:35 +0000

    mestre v.02

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -42,3 +42,39 @@ int main() {
     printf("Quantos pontos turísticos há? ");
     scanf("%d", &pontosTuristicos1);
     
+   // Cálculo dos indicadores da primeira carta
+   densidadePopulacional1 = populacao1 / area1;
+   pibPerCapita1 = (pib1 * 1000000000) / populacao1;
+   superPoder1 = populacao1 + area1 + (pib1 * 1000000000) + pontosTuristicos1 + pibPerCapita1 + (1.0 / densidadePopulacional1);
+   
+   // Entrada de dados para a segunda carta
+   printf("\n*** Carta 2 ***\n");
+   printf("Digite uma letra de (a-h): ");
+   scanf(" %c", &estado2);
+   
+   printf("Digite o código da carta (ex: A02): ");
+   scanf(" %3s", codigo2);
+   
+   getchar(); // Limpa o buffer antes de ler a string
+   printf("Digite o nome da cidade: ");
+   fgets(cidade2, sizeof(cidade2), stdin);
+   cidade2[strcspn(cidade2, "\n")] = '\0'; // Remove a quebra de linha
+   
+   printf("Digite a população: ");
+   scanf("%lu", &populacao2);
+   
+   printf("Digite o tamanho da área (km²): ");
+   scanf("%f", &area2);
+   
+   printf("Digite o PIB da cidade (em bilhões de reais): ");
+   scanf("%f", &pib2);
+   
+   printf("Quantos pontos turísticos há? ");
+   scanf("%d", &pontosTuristicos2);
+
+ // Cálculo dos indicadores da segunda carta
+ densidadePopulacional2 = populacao2 / area2;
+ pibPerCapita2 = (pib2 * 1000000000) / populacao2;
+ superPoder2 = populacao2 + area2 + (pib2 * 1000000000) + pontosTuristicos2 + pibPerCapita2 + (1.0 / densidadePopulacional2);
+ 
+ 
